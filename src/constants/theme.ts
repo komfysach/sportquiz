7f37fe42372a9f59ec0275107a3f7c24572d9bb4
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-02-14 10:22:14 +0100

    add theme file for app styling

---
+++ src/constants/theme.ts
@@ -0,0 +1,55 @@
+const theme = {
+  // Spacing
+  spacing2: '2px',
+  spacing4: '4px',
+  spacing5: '5px',
+  spacing6: '6px',
+  spacing8: '8px',
+  spacing10: '10px',
+  spacing12: '12px',
+  spacing15: '15px',
+  spacing20: '20px',
+  spacing25: '25px',
+  spacing30: '30px',
+  spacing35: '35px',
+  spacing40: '40px',
+
+  // Border radius
+  borderRadius5: '5px',
+  borderRadius10: '10px',
+  borderRadius15: '15px',
+  borderRadius20: '20px',
+
+  // Shadow
+  shadow: '3px 4px 0px rgba(0, 0, 0, 0.04)',
+
+  // Colors
+  greenBlack: '#1C1E1C',
+  purple: '#D992FF',
+  blue: '#3333FF',
+  green: '#A0ED5D',
+  lightGreen: '#CCFFCC',
+  white: '#ffffff',
+
+  // Font families (SpaceGrotesk)
+  SpaceGrotesqueBold: 'SpaceGrotesk-Bold',
+  SpaceGrotesqueMedium: 'SpaceGrotesk-Medium',
+  SpaceGrotesqueRegular: 'SpaceGrotesk-Regular',
+  SpaceGrotesqueLight: 'SpaceGrotesk-Light',
+
+  // Font families (Urbanist)
+  UrbanistBold: 'Urbanist-Bold',
+  UrbanistMedium: 'Urbanist-Medium',
+  UrbanistRegular: 'Urbanist-Regular',
+  UrbanistLight: 'Urbanist-Light',
+
+  // Font sizes
+  Heading32: '32px',
+  Heading24: '24px',
+  Heading18: '18px',
+  Paragraph16: '16px',
+  Paragraph14: '14px',
+  Paragraph12: '12px',
+};
+
+export default theme;
