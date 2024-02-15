1ae582d3d09b04f5f58cebc3d0f321ad4e276484
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-02-15 15:24:04 +0100

    add mock quiz data

---
+++ src/constants/quiz.ts
@@ -0,0 +1,151 @@
+export const quizzes = [
+  {
+    id: 1,
+    sport: 'Football',
+    levels: [
+      {
+        level: 1,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      {
+        level: 2,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      {
+        level: 3,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      // More levels...
+    ],
+  },
+  {
+    id: 2,
+    sport: 'Basketball',
+    levels: [
+      {
+        level: 1,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      // More levels...
+    ],
+  },
+  {
+    id: 3,
+    sport: 'Baseball',
+    levels: [
+      {
+        level: 1,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      // More levels...
+    ],
+  },
+  {
+    id: 4,
+    sport: 'Rugby',
+    levels: [
+      {
+        level: 1,
+        sections: [
+          {
+            section: 1,
+            questions: [
+              {
+                question: 'Question 1',
+                answers: [
+                  {answer: 'Answer 1', isCorrect: false, points: 0},
+                  {answer: 'Answer 2', isCorrect: true, points: 10},
+                  {answer: 'Answer 3', isCorrect: false, points: 0},
+                ],
+              },
+              // More questions...
+            ],
+          },
+          // More sections...
+        ],
+      },
+      // More levels...
+    ],
+  },
+  // More sports...
+];
