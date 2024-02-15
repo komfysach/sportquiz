f7bec141d36c4b5d553d07563131bae0129774e1
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-02-15 15:24:56 +0100

    WIP: Quiz page

--- src/screens/Home/components/Quiz/Quiz.tsx
+++ src/screens/Home/components/Quiz/Quiz.tsx
@@ -1,10 +1,48 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
+import {H1, H2} from '../../../../components/Typography/Headings';
+import Layout from '../../../../components/UI/Layout';
+import {quizzes} from '../../../../constants/quiz';
+import theme from '../../../../constants/theme';
 
-export default function Quiz() {
+const QuizContainer = styled.View`
+  gap: ${theme.spacing20};
+`;
+
+const Heading = styled(H1)`
+  margin-bottom: ${theme.spacing25};
+`;
+
+const LevelsWrapper = styled.View`\
+  gap: ${theme.spacing20};
+`;
+
+const Level = styled.View`
+  background-color: ${theme.lightGreen};
+  padding: ${theme.spacing20};
+  border-radius: ${theme.borderRadius20};
+`;
+
+const LevelHeading = styled(H2)`
+  color: ${theme.greenBlack};
+`;
+
+export default function Quiz({route}: {route: any}) {
+  const {id} = route.params;
+  const quiz = quizzes.find(quizId => quizId.id === id);
+  const [completedLevels, setCompletedLevels] = useState([1]);
   return (
-    <View>
-      <Text>Quiz</Text>
-    </View>
+    <Layout backLink>
+      <QuizContainer>
+        <Heading>{quiz?.sport}</Heading>
+        <LevelsWrapper>
+          {quiz?.levels.map(level => (
+            <Level key={level.level}>
+              <LevelHeading>Level {level.level}</LevelHeading>
+            </Level>
+          ))}
+        </LevelsWrapper>
+      </QuizContainer>
+    </Layout>
   );
 }
