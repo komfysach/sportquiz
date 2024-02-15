e4154b55407b57f112771764a14b31ef34eef86b
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-02-15 15:23:01 +0100

    update styling

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -3,16 +3,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {Dimensions} from 'react-native';
 import {
-  HeartIcon as HeartIconOutline,
   HomeIcon as HomeIconOutline,
   TrophyIcon as TrophyIconOutline,
-  UserCircleIcon as UserCircleIconOutline,
 } from 'react-native-heroicons/outline';
 import {
-  HeartIcon as HeartIconSolid,
   HomeIcon as HomeIconSolid,
   TrophyIcon as TrophyIconSolid,
-  UserCircleIcon as UserCircleIconSolid,
 } from 'react-native-heroicons/solid';
 import styled from 'styled-components/native';
 import theme from '../constants/theme';
@@ -24,7 +20,7 @@ const screenWidth = Dimensions.get('screen').width;
 
 const HomeWrapper = styled.View<{focused: Boolean}>`
   background-color: ${({focused}) =>
-    focused ? theme.lightGreen : 'transparent'};
+    focused ? theme.greenBlack : 'transparent'};
   padding: 10px;
   border-radius: 999px;
   margin-top: 10px;
@@ -41,11 +37,11 @@ export function getTabBarIcon(route: string, focused: boolean) {
         {focused ? (
           <HomeIconSolid
             testID="home-icon-solid"
-            color={theme.greenBlack}
+            color={theme.lightGreen}
             size={20}
           />
         ) : (
-          <HomeIconOutline color={theme.lightGreen} size={20} />
+          <HomeIconOutline color={theme.greenBlack} size={20} />
         )}
         {focused ? <Text focused={focused}>Home</Text> : null}
       </HomeWrapper>
@@ -57,45 +53,22 @@ export function getTabBarIcon(route: string, focused: boolean) {
         {focused ? (
           <TrophyIconSolid
             testID="trophy-icon-solid"
-            color={theme.greenBlack}
+            color={theme.lightGreen}
             size={20}
           />
         ) : (
-          <TrophyIconOutline color={theme.lightGreen} size={20} />
+          <TrophyIconOutline color={theme.greenBlack} size={20} />
         )}
         {focused ? <Text focused={focused}>Rankings</Text> : null}
       </HomeWrapper>
     );
   }
-  if (route === 'favourites') {
-    return (
-      <HomeWrapper focused={focused}>
-        {focused ? (
-          <HeartIconSolid color={theme.greenBlack} size={20} />
-        ) : (
-          <HeartIconOutline color={theme.lightGreen} size={20} />
-        )}
-        {focused ? <Text focused={focused}>Favourites</Text> : null}
-      </HomeWrapper>
-    );
-  }
-  if (route === 'profile') {
-    return (
-      <HomeWrapper focused={focused}>
-        {focused ? (
-          <UserCircleIconSolid color={theme.greenBlack} size={20} />
-        ) : (
-          <UserCircleIconOutline color={theme.lightGreen} size={20} />
-        )}
-        {focused ? <Text focused={focused}>Profile</Text> : null}
-      </HomeWrapper>
-    );
-  }
+
   return null;
 }
 
 const Text = styled.Text<{focused: Boolean}>`
-  color: ${({focused}) => (focused ? theme.greenBlack : theme.lightGreen)};
+  color: ${({focused}) => (focused ? theme.lightGreen : theme.greenBlack)};
   font-size: ${theme.Paragraph12};
   font-family: ${theme.UrbanistSemiBold};
   letter-spacing: 0.5px;
@@ -110,7 +83,7 @@ export default function Routes() {
           tabBarIcon: ({focused}) => getTabBarIcon(route.name, focused),
           tabBarLabel: () => null,
           tabBarStyle: {
-            width: screenWidth - 10, // set width to screen width
+            width: screenWidth - 100, // set width to screen width
             height: 70,
             paddingLeft: 7,
             paddingRight: 7,
@@ -118,15 +91,14 @@ export default function Routes() {
             borderRadius: 999, // add border radius
             position: 'absolute', // position it absolutely
             bottom: 10, // lift it up slightly from the bottom
-            left: 5, // align it to the left
-            backgroundColor: '#1C1E1C', // white background
+            left: 50, // align it to the left
+            right: 50, // align it to the right
+            backgroundColor: '#EEFFEE',
           },
         })}
         initialRouteName="home">
         <Tab.Screen name="home" component={HomeStack} />
         <Tab.Screen name="rankings" component={RankingsStack} />
-        <Tab.Screen name="favourites" component={RankingsStack} />
-        <Tab.Screen name="profile" component={RankingsStack} />
       </Tab.Navigator>
     </NavigationContainer>
   );
