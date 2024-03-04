dbcf070b109e146b96cd08882dbb30091a4df50d
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-04 15:22:35 +0100

    add get question with id action

---
+++ src/actions/getQuestionsWithId.ts
@@ -0,0 +1,24 @@
+import {QuestionType} from 'typings/QuestionType';
+import {supabase} from '../utils/superbase';
+
+export const getQuestionsWithId = async (sportId: number, levelId: number) => {
+  try {
+    const {data, error} = await supabase
+      .from('questions')
+      .select('*')
+      .eq('sport_id', sportId)
+      .eq('level_id', levelId);
+
+    if (error) {
+      console.error('Error fetching questions:', error);
+      // Handle error appropriately
+      return null;
+    }
+
+    return data as QuestionType[];
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    // Handle unexpected errors
+    return null;
+  }
+};
