d30fc29085eecb81a7cf59985de025f0060c71ac
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-04 15:19:35 +0100

    WIP: quiz

--- src/screens/Home/components/Quiz/Quiz.tsx
+++ src/screens/Home/components/Quiz/Quiz.tsx
@@ -1,12 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {LockClosedIcon} from 'react-native-heroicons/solid';
 import styled from 'styled-components/native';
 import {QuizLevelParamList} from 'typings/Navigation';
 import {H1, H3} from '../../../../components/Typography/Headings';
 import Layout from '../../../../components/UI/Layout';
-import {quizzes} from '../../../../constants/quiz';
 import theme from '../../../../constants/theme';
+import {getLevelsWithSportId} from '../../../../actions/getLevelsWithId';
+import {AppContext} from '../../../../context/AppContext';
+import {getUserProgress} from '../../../../actions/getUserProgress';
+import {LevelType} from 'typings/LevelType';
+import {updateUserProgress} from '../../../../actions/updateUserProgress';
+import {UserProgressType} from 'typings/UserProgressType';
 
 const QuizContainer = styled.View`
   gap: ${theme.spacing20};
@@ -16,7 +22,7 @@ const Heading = styled(H1)`
   margin-bottom: ${theme.spacing25};
 `;
 
-const LevelsWrapper = styled.View`\
+const LevelsWrapper = styled.View`
   gap: ${theme.spacing20};
 `;
 
@@ -39,31 +45,53 @@ const LockIcon = styled(LockClosedIcon)`
 
 export default function Quiz({route}: {route: any}) {
   const {id} = route.params;
-  const quiz = quizzes.find(quizId => quizId.id === id);
-  const [completedLevels, setCompletedLevels] = useState<Array<string>>([
-    'Simple - These are just tap ins',
-  ]);
+  const {levels, setLevels, sports, userProgress, setUserProgress, user} =
+    useContext(AppContext);
   const navigation = useNavigation<NavigationProp<QuizLevelParamList>>();
-  const handleLevelPress = (level: string) => {
-    if (completedLevels.includes(level)) {
-      navigation.navigate('QuizLevel', {id: id, level: level});
+  const quiz = sports?.find(sport => sport.sport_id === id);
+  const [completedLevels, setCompletedLevels] = useState<number[]>([1]);
+  const handleLevelPress = (level: LevelType) => {
+    if (completedLevels.includes(level.level_number)) {
+      navigation.navigate('QuizLevel', {sport: id, level: level?.level_number});
     } else {
       null;
     }
   };
 
+  const initialUserProgress = {
+    sport_id: id,
+    current_level: 1,
+    player_id: user?.id!,
+    points: 0,
+  };
+
+  useEffect(() => {
+    getUserProgress(user?.id!)
+      .then(data => {
+        if (data) {
+          if (JSON.stringify(data) !== JSON.stringify(initialUserProgress)) {
+            setUserProgress(data);
+          }
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user progress:', error);
+        // Handle error appropriately
+      });
+  }, []);
+
   return (
     <Layout backLink>
       <QuizContainer>
-        <Heading>{quiz?.sport}</Heading>
+        <Heading>{quiz?.sport_name}</Heading>
         <LevelsWrapper>
-          {quiz?.levels.map(level => (
+          {levels?.map(level => (
             <Level
-              onPress={() => handleLevelPress(level.level)}
-              notCompleted={!completedLevels.includes(level.level)}
-              key={level.level}>
-              <LevelHeading>Level {level.level}</LevelHeading>
-              {!completedLevels.includes(level.level) && (
+              onPress={() => handleLevelPress(level)}
+              notCompleted={!completedLevels.includes(level?.level_number)}
+              key={level?.level_number}>
+              <LevelHeading>Level {level?.level_number}</LevelHeading>
+              {!completedLevels.includes(level?.level_number) && (
                 <LockIcon size={24} color={theme.greenBlack} />
               )}
             </Level>
