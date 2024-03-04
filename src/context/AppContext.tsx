6ae07db3db4b9781bbcf9e6f1e6657f932ad194e
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-04 15:21:17 +0100

    update app context, add more states

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,9 +1,25 @@
-import React, {createContext, useEffect, useState} from 'react';
-import {supabase} from '../utils/initSuperbase';
+import React, {createContext, useState} from 'react';
+import {CategoryType} from 'typings/CategoryType';
+import {LevelType} from 'typings/LevelType';
+import {PlayerDataType} from 'typings/PlayerDataType';
+import {QuestionType} from 'typings/QuestionType';
+import {UserProgressType} from 'typings/UserProgressType';
 
 type AppContextType = {
   isFinishedOnboarding: boolean;
   setIsFinishedOnboarding: (isFinished: boolean) => void;
+  user: PlayerDataType | null;
+  setUser: (user: PlayerDataType) => void;
+  userProgress: UserProgressType[] | null;
+  setUserProgress: (userProgress: UserProgressType[]) => void;
+  players: PlayerDataType[] | null;
+  setPlayers: (players: PlayerDataType[]) => void;
+  sports: CategoryType[] | null;
+  setSports: (sports: CategoryType[]) => void;
+  levels: LevelType[] | null;
+  setLevels: (levels: LevelType[]) => void;
+  questions: QuestionType[] | null;
+  setQuestions: (questions: QuestionType[]) => void;
 };
 
 // create a context here
@@ -15,26 +31,32 @@ type AppProviderProps = {
 
 export function AppProvider({children}: AppProviderProps) {
   const [isFinishedOnboarding, setIsFinishedOnboarding] = useState(false);
-  const [quizData, setQuizData] = useState<any[]>([]);
-
-  useEffect(() => {
-    const getQuizData = async () => {
-      const {data: quiz} = await supabase.from('sport-categories').select();
-
-      if (quiz && quiz.length > 1) {
-        setQuizData(quiz!);
-      }
-    };
-
-    getQuizData();
-    console.log('quizData', quizData);
-  }, []);
+  const [user, setUser] = useState<PlayerDataType | null>(null);
+  const [sports, setSports] = useState<CategoryType[] | null>(null);
+  const [levels, setLevels] = useState<LevelType[] | null>(null);
+  const [questions, setQuestions] = useState<QuestionType[] | null>(null);
+  const [players, setPlayers] = useState<PlayerDataType[] | null>(null);
+  const [userProgress, setUserProgress] = useState<UserProgressType[] | null>(
+    null,
+  );
 
   return (
     <AppContext.Provider
       value={{
         isFinishedOnboarding,
         setIsFinishedOnboarding,
+        user,
+        setUser,
+        sports,
+        setSports,
+        levels,
+        setLevels,
+        questions,
+        setQuestions,
+        players,
+        setPlayers,
+        userProgress,
+        setUserProgress,
       }}>
       {children}
     </AppContext.Provider>
