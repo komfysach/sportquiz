be3a4db03a45cdf0a81af5e515ae0c253c25717e
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-04 15:18:36 +0100

    add question type

---
+++ src/typings/QuestionType.ts
@@ -0,0 +1,12 @@
+export type QuestionType = {
+  question_id: number;
+  question_text: string;
+  answer_a: string;
+  answer_b: string;
+  answer_c: string;
+  answer_d: string;
+  correct_answer: string;
+  points: number;
+  level_id: number;
+  sport_id: number;
+};
