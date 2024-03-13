e5be551e12f7830f5380f8fdb85864040237975d
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-13 08:23:51 +0100

    WIP: Rankings page

--- src/screens/Rankings/Rankings.tsx
+++ src/screens/Rankings/Rankings.tsx
@@ -1,10 +1,29 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import styled from 'styled-components/native';
+import {getPlayers} from '../../actions/getPlayers';
+import {H1} from '../../components/Typography/Headings';
+import Layout from '../../components/UI/Layout';
+
+const RankingContainer = styled.View`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Heading = styled(H1)`
+  font-size: 30px;
+  color: white;
+`;
 
 export default function RankingsStack() {
+  const [rankings, setRankings] = React.useState([]);
+  useEffect(() => {
+    getPlayers().then(res => console.log(res));
+  }, []);
   return (
-    <View>
-      <Text>RankingsStack</Text>
-    </View>
+    <Layout>
+      <RankingContainer>
+        <Heading>Rankings:</Heading>
+      </RankingContainer>
+    </Layout>
   );
 }
