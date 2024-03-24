d9f9263303c50f4b050cac180e22d6055aca0bbf
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-03-24 12:39:25 +0100

    remove Profile from HomeStack

--- src/screens/Home/HomeStack.tsx
+++ src/screens/Home/HomeStack.tsx
@@ -2,7 +2,6 @@ import {createStackNavigator} from '@react-navigation/stack';
 import React, {useContext} from 'react';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import Profile from './components/Profile/Profile';
 import Quiz from './components/Quiz/Quiz';
 import QuizLevel from './components/Quiz/components/QuizLevel';
 import {AppContext} from '../../context/AppContext';
@@ -10,21 +9,20 @@ import {AppContext} from '../../context/AppContext';
 const Stack = createStackNavigator();
 
 export default function HomeStack() {
-  const {isFinishedOnboarding} = useContext(AppContext);
+  const {isFinishedLoggingIn} = useContext(AppContext);
   return (
     <>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName={isFinishedOnboarding ? 'Home' : 'Login'}>
+        initialRouteName={isFinishedLoggingIn ? 'Home' : 'Login'}>
         <>
           <Stack.Group>
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Quiz" component={Quiz} />
             <Stack.Screen name="QuizLevel" component={QuizLevel} />
-            <Stack.Screen name="Profile" component={Profile} />
           </Stack.Group>
         </>
       </Stack.Navigator>
